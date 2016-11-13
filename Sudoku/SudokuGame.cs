ad0d7362a3d2dbc3091e32e2761b1954f87a1a26
Viet Tran <tphoangviet@gmail.com>
2016-11-13 00:38:50 +0000

    Initial commit

---
+++ Sudoku/SudokuGame.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class SudokuGame
+    {
+        public static void Main(string[] args)
+        {
+            var simpleTest = new int[9, 9] {
+
+                {0, 4, 0, 0, 0, 0, 1, 3, 2},
+                {0, 8, 3, 5, 1, 0, 0, 6, 0},
+                {7, 0, 0, 0, 3, 0, 4, 0, 0},
+                {0, 0, 4, 3, 9, 7, 0, 0, 0},
+                {0, 7, 0, 6, 0, 1, 0, 4, 0},
+                {0, 0, 0, 4, 2, 5, 3, 0, 0},
+                {0, 0, 2, 0, 5, 0, 0, 0, 4},
+                {0, 3, 0, 0, 6, 9, 5, 8, 0},
+                {1, 5, 6, 0, 0, 0, 0, 2, 0}
+            };
+
+            var evilTest = new int[9, 9] {
+
+                {7, 8, 2, 3, 0, 0, 0, 0, 0},
+                {0, 0, 9, 0, 0, 4, 0, 0, 0},
+                {3, 0, 6, 0, 0, 9, 0, 0, 0},
+                {0, 0, 8, 0, 1, 0, 0, 0, 6},
+                {6, 0, 0, 0, 0, 0, 0, 0, 5},
+                {2, 0, 0, 0, 4, 0, 1, 0, 0},
+                {0, 0, 0, 7, 0, 0, 6, 0, 2},
+                {0, 0, 0, 9, 0, 0, 8, 0, 0},
+                {0, 0, 0, 0, 0, 1, 7, 3, 9}
+            };
+
+            var suSol = new SudokuSolver { SudokuBoard = evilTest };
+
+            suSol.Solve(0, 0);
+
+            if (suSol.SudokuBoard != null)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        Console.Write(suSol.SudokuBoard[i,j] + (j < 8 ? " - " : ""));
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("\nBacktrack times: " + SudokuSolver.backTrackCount);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sudoku/SudokuSolver.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class SudokuSolver
+    {
+        private bool solutionFound = false;
+
+        public static int backTrackCount = 0;
+        
+        public int[,] SudokuBoard { get; set; }
+
+        private bool ValidInRow(int row, int value)
+        {
+            for (int col = 0; col < 9; col++)
+            {
+                if (SudokuBoard[row, col] == value)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool ValidInCol(int col, int value)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                if (SudokuBoard[row, col] == value)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool ValidInBlock(int row, int col, int value)
+        {
+            row = (row / 3) * 3;
+            col = (col / 3) * 3;
+
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    if (SudokuBoard[row + r, col + c] == value)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public void Solve(int row, int col)
+        {
+            if (row > 8)
+            {
+                solutionFound = true;
+                return;
+            }
+            else
+            {
+                while (SudokuBoard[row, col] != 0)
+                {
+                    if (++col > 8)
+                    {
+                        col = 0;
+                        row++;
+
+                        if (row > 8)
+                        {
+                            solutionFound = true;
+                            return;
+                        }
+                    }
+                }
+                
+                for (int val = 1; val < 10; val++)
+                {
+                    if (ValidInRow(row, val) && 
+                        ValidInCol(col, val) && ValidInBlock(row, col, val))
+                    {
+                        SudokuBoard[row, col] = val;
+
+                        if (col < 8)
+                        {
+                            Solve(row, col + 1);
+                        }
+                        else
+                        {
+                            Solve(row + 1, 0);
+                        }
+
+                        backTrackCount++;
+                    }
+                }
+
+                if (!solutionFound)
+                {
+                    SudokuBoard[row, col] = 0;
+                }
+            }
+        }
+    }
+}
